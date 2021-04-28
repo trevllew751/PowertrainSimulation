92f39fb8681805d9b0eba33beac5409b9e83ad54
Trevor <trevllew751@vt.edu>
2021-04-28 18:23:54 -0400

    Script now generates pack configurations based off of motor controller specs

--- PowertrainV2/PowertrainSim.py
+++ PowertrainV2/PowertrainSim.py
@@ -5,14 +5,14 @@ import csv
 
 # Uses OpenPyXL engine to parse the workbook into a dictionary/dataframe where the keys are the individual sheet names
 parse = pd.read_excel("Powertrain Simulation Data.xlsx", engine="openpyxl",
-                      sheet_name=["Motors", "Motor_Controllers", "Batteries"])
+                      sheet_name=["Motors", "MotorControllers", "Batteries"])
 # Breaks the parse into individual sheets. The sheet types are dataframes
 motors = parse["Motors"].values
-motor_controllers = parse["Motor_Controllers"].values
+motor_controllers = parse["MotorControllers"].values
 batteries = parse["Batteries"].values
 
 
-def all_batt_configs(v_min, i_min, v_max, i_max):
+def all_batt_configs(controller, v_min, i_min, v_max, i_max):
     """ Takes a minimum and maximum voltage and current, parses the batteries spreadsheet, and generates the minimum
         viable pack for the required voltage and current
         Battery, Nominal Voltage (V), Nominal Capacity (Ah), Cell Weight (g), Internal Resistance (Ohms), Cost (USD)"""
@@ -23,6 +23,7 @@ def all_batt_configs(v_min, i_min, v_max, i_max):
         for v in np.arange(v_min, v_max, 1):
             for i in np.arange(i_min, i_max, 1):
                 cells_series = np.ceil(v / nom_voltage)
+                # TODO: Fix parallel cells calculations
                 cells_parallel = np.ceil(i / nom_capacity)
                 total_cells = cells_series * cells_parallel
                 pack = {"cell_type": str(battery[0]),
@@ -37,7 +38,7 @@ def all_batt_configs(v_min, i_min, v_max, i_max):
                         "weight (lbs)": np.round((total_cells * battery[3]) / 1000 * 2.2, 2),
                         "cost (USD)": total_cells * battery[5] if battery[5] else "N/A"}
                 packs.append(pack)
-        with open("packs.csv", 'w', newline='') as packs_csv:
+        with open(f"./packs/{controller}.csv", 'w', newline='') as packs_csv:
             fieldnames = ["cell_type", "cells_total", "cells_series", "cells_parallel", "pack_voltage",
                           "pack_current", "capacity (kWh)", "resistance_total", "weight (lbs)", "cost (USD)"]
             writer = csv.DictWriter(packs_csv, fieldnames=fieldnames)
@@ -46,8 +47,15 @@ def all_batt_configs(v_min, i_min, v_max, i_max):
                 writer.writerow(pack)
 
 
+def motor_controller_configs():
+    for mc in motor_controllers:
+        #                mc model    v_min  i_min  v_max  i_max
+        all_batt_configs(str(mc[0]), mc[1], mc[3], mc[2], mc[4])
+
+
 def main():
-    all_batt_configs(100, 100, 105, 110)  # Dummy test
+    # all_batt_configs(100, 100, 105, 110)  # Dummy test
+    motor_controller_configs()
 
 
 if __name__ == "__main__":
